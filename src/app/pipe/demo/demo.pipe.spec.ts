f9a8f9a27de80a995c3a588bd033a6003aee714b
Charlton Cheng <charltoncheng86@gmail.com>
2018-03-15 14:06:29 +1100

    Isolated demo code for Pipe

--- src/app/pipe/demo/demo.pipe.spec.ts
+++ src/app/pipe/demo/demo.pipe.spec.ts
@@ -1,18 +1,51 @@
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+
 import { DemoPipe } from './demo.pipe';
+import { DummyComponent } from '../../ui-component/cmp/dummy/dummy.component';
 
 describe('DemoPipe', () => {
     let pipe;
-    beforeEach(() => {
-        pipe = new DemoPipe();
+
+    describe('Test by itself', () => {
+        beforeEach(() => {
+            pipe = new DemoPipe();
+        });
+
+        it('create an instance', () => {
+            expect(pipe).toBeTruthy();
+        });
+
+        it('should convert to lowercase', () => {
+            const original = 'ABC';
+            const result = pipe.transform(original);
+            expect(result).toBe('abc');
+        });
     });
 
-  it('create an instance', () => {
-    expect(pipe).toBeTruthy();
-  });
+    describe('Test with Component', () => {
+        let cmpFixture: ComponentFixture<DummyComponent>,
+            cmpTplElem: any;
 
-  it('should convert to lowercase', () => {
-    const original = 'ABC';
-    const result = pipe.transform(original);
-    expect(result).toBe('abc');
-  });
+        beforeEach(async(() => {
+            TestBed.configureTestingModule({
+                declarations: [
+                    DemoPipe,
+                    DummyComponent
+                ]
+            });
+        }));
+
+        beforeEach(() => {
+            const option = { set: { template: `<p>{{ 'ABC' | demoPipe}}</p>` }};
+            cmpFixture = TestBed.overrideComponent(DummyComponent, option).createComponent(DummyComponent);
+            cmpTplElem = cmpFixture.debugElement.nativeElement;
+
+            cmpFixture.detectChanges();
+        });
+
+        it('should contain converted lowercase `abc`', () => {
+            expect(cmpTplElem.textContent).toContain('abc');
+        });
+    });
+
 });
